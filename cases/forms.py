641da5d38b7ba2f080970bd62bb5fbacaff8dcb1
mostafaElkenany <mostafa.elkenany@gmail.com>
2020-06-07 22:56:35 +0200

    -added validation on age, jail time and convection date in create new case form

--- cases/forms.py
+++ cases/forms.py
@@ -18,11 +18,29 @@ class AddCaseForm(forms.ModelForm):
             "total_target",
             "details",
         )
+
         error_messages = {
             'total_target': {
-                'min_value': "Invalid value, target must be greater than zero",
+                'min_value': "Total target must be greater than zero",
+            },
+            'jail_time': {
+                'min_value': "jail time must be greater than or equal to one year",
+            },
+            'no_of_dependents': {
+                'min_value': "Number of dependents must be greater than or equal to zero",
+            },
+            'age': {
+                'min_value': "Age must be 18 or above",
             },
         }
+
         widgets = {
             "convection_date": forms.DateInput(attrs={"type": "date"}),
         }
+        
+    def clean(self):
+        cleaned_data = super().clean()
+        convection_date = cleaned_data.get("convection_date")
+        if convection_date > datetime.date.today():
+            msg = "Convection date should be before or equal current date."
+            self.add_error("convection_date", msg)
